264940e9f16ca8750e32af33350a4be86665c2f2
ROHIT RAWAT <rohit@spoton.com>
2019-03-24 20:52:03 -0600

    Added second functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -36,7 +36,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys(text)
         inputbox.send_keys(Keys.ENTER)
-    
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
@@ -50,4 +50,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.enter_text_inputbox('Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('Finish the test!')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        
+        self.enter_text_inputbox('Buy peacock feathers')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        first_list_url = self.browser.current_url
+        self.assertRegex(first_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        self.enter_text_inputbox('Buy Milk')
+        self.check_for_row_in_list_table('1: Buy Milk')
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
