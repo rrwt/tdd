1d64c6e01ed7735e39b1541186599f9d561cd6d5
ROHIT RAWAT <rohit@spoton.com>
2019-03-26 12:50:50 -0600

    refactored url file

---
+++ lists/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from lists import views
+
+urlpatterns = [
+    path(r'new', views.new_list, name='new_list'),
+    path(r'<int:list_id>/', views.view_list, name='view_list'),
+    path(r'<int:list_id>/add_item', views.add_item, name='add_item'),
+]
--- superlists/urls.py
+++ superlists/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include, url
 
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path(r'lists/new', views.new_list, name='new_list'),
-    path(r'lists/<int:list_id>/', views.view_list, name='view_list'),
-    path(r'lists/<int:list_id>/add_item', views.add_item, name='add_item'),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'lists/', include(list_urls))
 ]
