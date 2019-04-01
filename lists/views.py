5137db79b3a71ae00d9956f55ed7fa33084acbdc
ROHIT RAWAT <rohit@spoton.com>
2019-03-31 19:36:19 -0600

    updated form to save item

--- lists/views.py
+++ lists/views.py
@@ -19,7 +19,7 @@ def view_list(request, list_id):
         form = ItemForm(data=request.POST)
 
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
 
     return render(request, 'list.html', {'list': list_, 'form': form})
@@ -30,7 +30,7 @@ def new_list(request):
 
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
